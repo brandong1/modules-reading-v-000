e0fc846ca276b2e6cde962bdb5ac61ec55714aa4
Brandon Green <brandon@brandonleeproductions.com>
2018-06-27 02:30:25 +0000

    automatically backed up by learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,8 +2,8 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
-
   include Dance # To lend our two classes all of the methods of the Dance module, we use the include keyword
+  extend MetaDancing
 
   attr_accessor :name
 

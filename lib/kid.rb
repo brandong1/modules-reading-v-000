b8c021323bddf9e66b44267d76fab0709bbf5cdf
Brandon Green <brandon@brandonleeproductions.com>
2018-06-27 02:21:32 +0000

    automatically backed up by learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,8 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+
+class Kid
+
+  include Dance # To lend our two classes all of the methods of the Dance module, we use the include keyword
+
+end
